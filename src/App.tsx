e33f4ecdad532001f0a09c19c31eee6538b5cd64
Razskler <raslarnhort@gmail.com>
2026-03-22 19:11:56 +0300

    I'm currently building the job post structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
+interface JobPost {
+  id: nubmer;
+  company: string;
+  logo: string;
+  position: string;
+  role: string;
+  level: string;
+  description: string;
+  languages: string[];
+}
+
+const [jPosts, setJposts] = useState<JobPosts[]>(data);
+const [filters, setFilters] = useState<string[]>([]);
+
 function App() {
   const [count, setCount] = useState(0);
 
