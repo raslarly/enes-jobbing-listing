d133c39e0cdadcdcd71c2cc9e3581482ed27fe5c
Razskler <raslarnhort@gmail.com>
2026-03-25 13:02:26 +0300

    I'm done with X!

--- src/App.tsx
+++ src/App.tsx
@@ -115,18 +115,21 @@ function App() {
         {filters.length > 0 && (
           <div className=" -mt-[2rem] min-h-[4rem]  max-w-[1360px] bg-white p-[0.5] rounded-[0.35rem] flex flex-row flex-wrap gap-[0.5rem] items-center filter-bar">
             {filters.map((f) => (
-              <div className=" p-[0.1rem] rounded-[0.25rem] filter" key={f}>
+              <div
+                className=" p-[0.1rem] rounded-[0.25rem] flex items-center filter"
+                key={f}
+              >
                 <span
                   className="bg-[#5ba4a4] border-none cursor-pointer p-[0.15rem] radius-[0.375rem] text-white 
                 text-[0.8rem] font-family-inherit transition
-                duration[0.2s] hover:bg-[#488484]"
+                duration[0.2s] hover:bg-[#488484] rounded-tl-[0.25rem] rounded-bl-[0.25rem] "
                 >
                   {f}
                 </span>
                 <button onClick={() => removeFilter(f)}>
                   <img
                     src={IconRemove}
-                    className=" p-[0.75rem] w-4 h-4 bg-[#006e1f]"
+                    className=" p-[0.4rem] rounded-br-[0.25rem] rounded-tr-[0.25rem] w-7 h-7 bg-[#488484]"
                   />
                 </button>
               </div>
